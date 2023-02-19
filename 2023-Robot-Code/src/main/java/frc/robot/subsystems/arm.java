8938b626b5fe7a8b8ca3a5d605058aa190bcabef
Ar-ael-Anaya <pinguis10.ar.3@gmail.com>
2023-02-19 16:55:47 -0700

    Updated Arm Subsystem

--- 2023-Robot-Code/src/main/java/frc/robot/subsystems/arm.java
+++ 2023-Robot-Code/src/main/java/frc/robot/subsystems/arm.java
@@ -9,10 +9,30 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class arm extends SubsystemBase {
+	private double ArmAngle = 0;
+
+	public void setArmAngle(double value) {
+		ArmAngle = value;
+	}
+
+	public double getArmAngle() {
+		return ArmAngle;
+	}
+
+	public void reduceArmAngle(double amount) {
+		ArmAngle = ArmAngle - amount;
+	}
+
+	public void increaseArmAngle(double amount) {
+		ArmAngle = ArmAngle + amount;
+	}
 
 	PWMVictorSPX armMotor = new PWMVictorSPX(Constants.Motors.arm.armMotor);
 
-	/** Creates a new arm. */
+	public void setMotorSpeed(double axis) {
+		armMotor.set(axis);
+	}
+
 	public arm() {}
 
 	@Override
@@ -20,7 +40,5 @@ public class arm extends SubsystemBase {
 		// This method will be called once per scheduler run
 	}
 
-	public void setMotorSpeed(double axis) {
-		armMotor.set(axis);
-	}
+	
 }
