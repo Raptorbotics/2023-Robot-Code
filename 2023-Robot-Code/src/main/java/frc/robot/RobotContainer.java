02377d0e6c5816ff998c874bb262cfe97f6cc5ec
Impxrtant322 <63988752+Impxrtant322@users.noreply.github.com>
2023-02-19 01:42:49 -0700

    initial start

--- 2023-Robot-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -5,11 +5,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Robot;
 import frc.robot.commands.Drive;
 import frc.robot.commands.ShoulderTeleop;
 
