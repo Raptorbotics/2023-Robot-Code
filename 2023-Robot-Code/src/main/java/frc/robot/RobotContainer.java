8d8ba9f2196d397c38c020b1bb04d1b03f48b190
Impxrtant322 <63988752+Impxrtant322@users.noreply.github.com>
2023-02-18 18:08:11 -0700

    commented keybind

--- 2023-Robot-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -56,11 +56,11 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the trigger bindings
      
-    DOWN.whileTrue(new ShoulderTeleop("Manual Down"));
+    //ShoulderTeleop Keybinds
+    DOWN.whileTrue(new ShoulderTeleop("Manual Down")); 
     rBumper.and(aButton).onTrue(new ShoulderTeleop("Low"));
     rBumper.and(bButton).onTrue(new ShoulderTeleop("Medium"));
     rBumper.and(yButton).onTrue(new ShoulderTeleop("High"));
-
     UP.whileTrue(new ShoulderTeleop("Manual Up"));
     configureBindings();
   }
